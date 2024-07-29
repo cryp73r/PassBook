39c334a3b33ef5888c518dc7bdcccad54ffaf778
Priyanshu Singh <priyanshusingh0610@gmail.com>
2024-07-30 02:19:16 +0530

    Now passing extra parameter to service class functions of username for better security

--- src/main/java/com/cryp73r/passbook/controller/CredentialController.java
+++ src/main/java/com/cryp73r/passbook/controller/CredentialController.java
@@ -1,13 +1,14 @@
 package com.cryp73r.passbook.controller;
 
+import com.cryp73r.passbook.dto.CredentialDTO;
 import com.cryp73r.passbook.model.Credential;
-import com.cryp73r.passbook.security.jwt.JwtUtils;
 import com.cryp73r.passbook.service.CredentialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -18,34 +19,37 @@ import java.net.URI;
 public class CredentialController {
 
     @Autowired
-    private JwtUtils jwtUtils;
-
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Autowired
     CredentialService credentialService;
 
-    @PreAuthorize("hasRole('USER')")
+    @PreAuthorize("hasRole('GENERAL_USER')")
     @PostMapping(value = "/credential", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Credential> addCredential(@RequestBody Credential credential) {
-        Credential savedCredential = credentialService.addCredential(credential, "test");
+    public ResponseEntity<CredentialDTO> addCredential(@RequestBody CredentialDTO credentialDTO) {
+        CredentialDTO savedCredential = credentialService.addCredential(credentialDTO, getAuthenticatedUsername());
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
-                .buildAndExpand(savedCredential.getRowid()).toUri();
+                .buildAndExpand(savedCredential.getRowId()).toUri();
         return ResponseEntity.created(location).body(savedCredential);
     }
 
-    @PreAuthorize("hasRole('USER')")
+    @PreAuthorize("hasRole('GENERAL_USER')")
     @GetMapping(value = "/credential/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Credential> getCredential(@PathVariable(name = "id") Long rowId) {
-        return ResponseEntity.ok(credentialService.getCredential(rowId));
+    public ResponseEntity<CredentialDTO> getCredential(@PathVariable(name = "id") Long rowId) {
+        return ResponseEntity.ok(credentialService.getCredential(rowId, getAuthenticatedUsername()));
     }
 
-    @PreAuthorize("hasRole('USER')")
+    @PreAuthorize("hasRole('GENERAL_USER')")
     @DeleteMapping("/credential/{id}")
     public ResponseEntity<Credential> deleteCredential(@PathVariable(name = "id") Long rowId) {
-        credentialService.deleteCredential(rowId);
+        credentialService.deleteCredential(rowId, getAuthenticatedUsername());
         return ResponseEntity.noContent().build();
+    }
+
+    private String getAuthenticatedUsername() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            return ((UserDetails) principal).getUsername();
+        } else {
+            return principal.toString();
+        }
     }
 }
