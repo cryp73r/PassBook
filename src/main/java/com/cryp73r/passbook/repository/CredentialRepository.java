fb148d658e9e4092df46150d2f91422e768e02a5
Priyanshu Singh <priyanshusingh0610@gmail.com>
2024-07-30 02:18:25 +0530

    Correcte the function to fetch credential based on id and owner and similarly for delete operation

--- src/main/java/com/cryp73r/passbook/repository/CredentialRepository.java
+++ src/main/java/com/cryp73r/passbook/repository/CredentialRepository.java
@@ -2,8 +2,17 @@ package com.cryp73r.passbook.repository;
 
 import com.cryp73r.passbook.model.Credential;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CredentialRepository extends JpaRepository<Credential, Long> {
+
+    @Query("SELECT crdtl FROM Credential crdtl WHERE crdtl.rowId = :id AND crdtl.owner = :owner")
+    Optional<Credential> findByIdAndOwner(Long id, String owner);
+
+    @Query("DELETE FROM Credential crdtl WHERE crdtl.rowId = :id AND crdtl.owner = :owner")
+    void deleteByIdAndOwner(Long id, String owner);
 }
